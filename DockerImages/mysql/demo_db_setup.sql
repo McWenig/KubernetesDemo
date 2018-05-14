91f4d25abb49ee98a5f5440d1eb14eee1e3f0b51
Martin Wenig <martin.wenig@accenture.com>
2018-05-14 08:55:08 +0200

    DockerImages for simple mysql-Image with a pre-populated demo_db

---
+++ DockerImages/mysql/demo_db_setup.sql
@@ -0,0 +1,28 @@
+Use demo_db;
+
+CREATE TABLE  act (
+`id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+name Varchar(128) not null
+);
+
+CREATE TABLE  protocoll (
+`id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+name Varchar(128) not null,
+act_id INT,
+INDEX act_ind (act_id),
+    FOREIGN KEY (act_id)
+        REFERENCES act(id)
+);
+
+insert into act set name='test1';
+insert into act set name='test2';
+insert into act set name='test3';
+insert into act set name='test4';
+
+insert into protocoll (name, act_id) select name,id from act where name='test1';
+insert into protocoll (name, act_id) select name,id from act where name='test2';
+insert into protocoll (name, act_id) select name,id from act where name='test3';
+insert into protocoll (name, act_id) select name,id from act where name='test4';
+
+select * From act;
+select * from protocoll;
